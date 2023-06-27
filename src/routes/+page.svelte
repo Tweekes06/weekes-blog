55ce99194e8a3cc824ed125f6cb13cc49ee55796
Tweekes06 <weekes26@gmail.com>
2023-06-27 14:44:54 +0100

    nav bar added and each sub page has its own title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<h1>This is Tom's site</h1>
-<p>And this is a subheadding</p>
-
-<style>
-	h1 {
-		font-family: 'Brush Script MT', cursive;
-		color: firebrick;
-	}
-	p {
-		font-family: 'copperplate', fantasy;
-		color: blue;
-	}
-</style>
+<h1>Home</h1>
