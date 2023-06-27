2634f84e527517dd6dcd72caf6a0f634e041f0ea
Tweekes06 <weekes26@gmail.com>
2023-06-27 15:28:24 +0100

    images add and a welcome meassage is now displayed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
 <h1>Home</h1>
+<h2>Welcome to my blog</h2>
+<img src="/fs_me.jpg" alt="A pictutre of me in flying gear" width="300" />
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,6 @@
 <h1>About</h1>
+<h3>Education</h3>
+<p>
+	I am a student at exeterschool studying Maths, Further maths, Physics and electronics A-levels.
+</p>
+<img src="/me.jpg" alt="A pictutre of me" width="300" />
