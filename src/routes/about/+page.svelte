74d40f508b955d3dc5ee6eb0a094aba319dd2479
Tweekes06 <weekes26@gmail.com>
2023-06-27 16:09:11 +0100

    updates the about page and the cats page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,7 @@
 <h1>About me</h1>
+<h2>Hobbies and past times</h2>
+<p>
+	I like planes and computers and I like to spend my past time disasembeling various items to either
+	fix them or gain a greater understanding of how things work
+</p>
 <img src="/me.jpg" alt="A pictutre of me" width="300" />
--- src/routes/cats/+page.svelte
+++ src/routes/cats/+page.svelte
@@ -1,4 +1,9 @@
 <h1>A cat photo dump</h1>
+<p>
+	Featured on this page are 3 cats. The ginger black and white one is the oldest and she is called
+	Tinky, she hates the other two. The other two cats are younger and are brothers. The black and
+	white cat is phoenix and he is a bit stupid and then the other one is lando and his is very small
+</p>
 <img src="/cat (1).jpg" alt="A pictutre of 3 cats sitting in the sunset" width="300" />
 <img src="/cat (2).jpg" alt="A pictutre of 3 cats sitti ng in the sunset" width="300" />
 <img src="/cat (3).jpg" alt="A pictutre of a cat sitting in the sunset" width="300" />
