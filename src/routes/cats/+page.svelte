e6c4545d23e8b32b2da5754a7836423e695617b8
Tweekes06 <weekes26@gmail.com>
2023-06-27 16:03:19 +0100

    alt descriptions of cats images improved

--- src/routes/cats/+page.svelte
+++ src/routes/cats/+page.svelte
@@ -1,8 +1,8 @@
 <h1>A cat photo dump</h1>
-<img src="/cat (1).jpg" alt="A pictutre of cat(s)" width="300" />
-<img src="/cat (2).jpg" alt="A pictutre of cat(s)" width="300" />
-<img src="/cat (3).jpg" alt="A pictutre of cat(s)" width="300" />
-<img src="/cat (4).jpg" alt="A pictutre of cat(s)" width="300" />
+<img src="/cat (1).jpg" alt="A pictutre of 3 cats sitting in the sunset" width="300" />
+<img src="/cat (2).jpg" alt="A pictutre of 3 cats sitti ng in the sunset" width="300" />
+<img src="/cat (3).jpg" alt="A pictutre of a cat sitting in the sunset" width="300" />
+<img src="/cat (4).jpg" alt="A pictutre of a cat sitting in the sunset" width="300" />
 <img src="/cat (5).jpg" alt="A pictutre of a stupid cat" width="300" />
-<img src="/cat (6).jpg" alt="A pictutre of cat(s)" width="300" />
-<img src="/cat (7).jpg" alt="A pictutre of cat(s)" width="300" />
+<img src="/cat (6).jpg" alt="A pictutre of 2 cats on a chair" width="300" />
+<img src="/cat (7).jpg" alt="A pictutre of 2 cats on a chair" width="300" />
