df1a3c1b1546a6c7ff81d6236da4668e3404e84e
Tweekes06 <weekes26@gmail.com>
2023-06-28 10:37:20 +0100

    changed thales nave bar link

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,7 +3,7 @@
 		<li><a href="/">Home</a></li>
 		<li><a href="/about">About</a></li>
 		<li><a href="/contact">Contact</a></li>
-		<li><a href="/Thales">Thales</a></li>
+		<li><a href="/thales">Thales</a></li>
 		<li><a href="/cats">Cats</a></li>
 	</ul>
 </nav>
