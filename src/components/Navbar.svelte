e3ab510a47740ba6511763c1491baddeb1fae67e
Tweekes06 <weekes26@gmail.com>
2023-06-28 15:22:09 +0100

    Nav bar centre justified and made bold

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,12 @@
 <nav>
-	<ul class="flex space-x-4 bg-blue-500">
-		<li><a href="/" class="hover:text-green-900 transition-colors">Home</a></li>
-		<li><a href="/about" class="hover:text-green-900 transition-colors">About</a></li>
-		<li><a href="/contact" class="hover:text-green-900 transition-colors">Contact</a></li>
-		<li><a href="/thales" class="hover:text-green-900 transition-colors">Thales</a></li>
-		<li><a href="/cats" class="hover:text-green-900 transition-colors">Cats</a></li>
-		<li><a href="/button" class="hover:text-green-900 transition-colors">The Button</a></li>
+	<ul
+		class="text-2xl flex space-x-4 pt-4 bg-blue-500 items-center justify-center font-semibold text-slate-800"
+	>
+		<li><a href="/" class="hover:text-red-950 transition-colors">Home</a></li>
+		<li><a href="/about" class="hover:text-red-950 transition-colors">About</a></li>
+		<li><a href="/contact" class="hover:text-red-950 transition-colors">Contact</a></li>
+		<li><a href="/thales" class="hover:text-red-950 transition-colors">Thales</a></li>
+		<li><a href="/cats" class="hover:text-red-950 transition-colors">Cats</a></li>
+		<li><a href="/button" class="hover:text-red-950 transition-colors">The Button</a></li>
 	</ul>
 </nav>
