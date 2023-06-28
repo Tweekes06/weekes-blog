d92e5cc0fb92416843d281d98953922ac69f15e9
Tweekes06 <weekes26@gmail.com>
2023-06-28 10:41:03 +0100

    Thales page updated

--- src/routes/thales/+page.svelte
+++ src/routes/thales/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Thales</h1>
-<p>I did my first work experience at Thales and that was where this site was first developed.</p>
-
+<p>I did my first work experience at the Thales site in Templecombe and that was where this site was first developed.</p>
+<h2>What I learned</h2>
+<p>I learned about using svelte to create web pages useing javascript as the language.</p>
