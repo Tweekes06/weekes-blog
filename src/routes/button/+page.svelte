a7988ed0bed69cefcc00d9a150f9d7c0836b1f00
Tweekes06 <weekes26@gmail.com>
2023-06-28 11:24:31 +0100

    button page added

--- src/routes/button/+page.svelte
+++ src/routes/button/+page.svelte
@@ -1,11 +1,23 @@
 <script>
+	import { parse } from 'svelte/compiler';
+
 	let count = 0;
+	let maxValue = null;
+	let x = 0;
 
 	function increment() {
 		count++;
 	}
 
-	$: if (count >= 10) {
+	while (maxValue == null) {
+		maxValue = prompt('what is the maximum amount of times the box can be clicked?');
+		if (maxValue != null) {
+			x = parseInt(maxValue);
+			x = x++;
+		}
+	}
+
+	$: if (count == x) {
 		alert('The button has been pressed too many times');
 		count = 0;
 	}
