fa5eb9ab0493720fc00be20175d301bd9f5036bb
Tweekes06 <weekes26@gmail.com>
2023-06-28 10:48:15 +0100

    improvements to the thales page and a image

--- src/routes/thales/+page.svelte
+++ src/routes/thales/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Thales</h1>
+<img src="/Thales_Logo.svg.png" alt="The Thales logo" width="1800"/>
 <p>I did my first work experience at the Thales site in Templecombe and that was where this site was first developed.</p>
 <h2>What I learned</h2>
 <p>I learned about using svelte to create web pages useing javascript as the language.</p>
