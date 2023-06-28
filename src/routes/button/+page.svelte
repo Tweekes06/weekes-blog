8230ac1145a350923c59c361dfa1e2fc3ee56ce5
Tweekes06 <weekes26@gmail.com>
2023-06-28 11:58:04 +0100

    improvements to button page

--- src/routes/button/+page.svelte
+++ src/routes/button/+page.svelte
@@ -1,22 +1,20 @@
 <script>
 	import { browser } from '$app/environment';
-	import { parse } from 'svelte/compiler';
 
 	let count = 0;
 	let maxValue = null;
 	let x = 0;
 
-	function increment() {
-		count++;
-	}
-
-	while (maxValue == null && browser) {
+	function getValue() {
 		maxValue = prompt('what is the maximum amount of times the box can be clicked?');
 		if (maxValue != null) {
 			x = parseInt(maxValue);
 			x++;
+			count = 0;
 		}
 	}
+
+	while (maxValue == null && browser) getValue();
 
 	$: if (count == x) {
 		if (browser) alert('The button has been pressed too many times');
@@ -26,7 +24,10 @@
 
 <h1>The button</h1>
 
-<button on:click={increment}>
+<button on:click={() => count++}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
+
+<button on:click={getValue}> Adjust max value </button>
+<button on:click={() => (count = 0)}>Reset button to 0</button>
